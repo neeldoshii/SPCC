c6f68be5c5644788ee6b9e9d089b102adc6245d0
Neel Doshi <neeldoshi147@gmail.com>
2024-04-30 00:32:04 +0530

    removed

--- 2-pass/pass2.c
+++ 2-pass/pass2.c
@@ -3,37 +3,39 @@
 #include<string.h>
 void main()
 {
-    FILE *f1,*f2,*f3,*f4;
-    char ch[100],chp[100];
-    int addr=1000;
-    f1=fopen("input1.txt","r");
-    f2=fopen("optab1.txt","r");
-    f3=fopen("littab2.txt","w");
-    f4=fopen("symtab2.txt","w");
-    while (fscanf(f1,"%s",ch) !=EOF)
-    {
-        if(strcmp(ch,"-")==0)
-        {
-            addr++;
-        }
-        if(strcmp(ch,"db")==0)
-        {
-            fprintf(f4,"%s\t%d\n",chp,addr);
-        }
-        if(strcmp(chp,"LTORG")==0)
-        {
-            fprintf(f3,"%s\t%d\n",ch,addr);
-        }
-        strcpy(chp,ch);
-    }
-    fclose(f4);
-    fclose(f3);
-    fclose(f2);
-    fclose(f1);
+FILE *f1,*f2,*f3,*f4;
+char ch[100],chp[100];
+int addr=1000;
+int as=5;
+int bs=2;
+f1=fopen("input.txt","r");
+f2=fopen("optab.txt","r");
+f3=fopen("littab2.txt","w");
+f4=fopen("symtab2.txt","w");
+while (fscanf(f1,"%s",ch) !=EOF)
+{
+if(strcmp(ch,"-")==0)
+{
+addr++;
+}
+if(strcmp(ch,"db")==0)
+{
+fprintf(f4,"%s\t%d\n",chp,addr);
+}
+if(strcmp(chp,"LTORG")==0)
+{
+fprintf(f3,"%s\t%d\n",ch,addr);
+}
+strcpy(chp,ch);
+}
+fprintf(f3,"%d\n%d\n",as,bs);
+fclose(f4);
+fclose(f3);
+fclose(f2);
+fclose(f1);
 }
 
 /*
-input1
 ** START 2000
 2000 ** LDA FIVE
 2003 ** STA ALPHA
@@ -44,36 +46,4 @@ input1
 2021 CHARZ BYTE Z
 2023 C1 RESB
 2024 ** END **
-*/
-
-/*
-optab1
-LDA 03
-LDA 0f
-LDCH 53
-STCH 57
-END *
-*/
-
-/*
-littab2
-1 Z 2018
-*/
-
-/*
-symtab2
-2000 **
-2003 **
-2006 **
-2009 **
-2012 ALPHA
-2018 FIVE
-2021 CHARZ
-2023 C1
-2024 **
-*/
-
-/*
-gcc pass2.c -o pass2 
-./pass2
 */
